d757f096866babb9fda7261325ca572187a14f81
Marcos Monteiro <markosmonteiro@gmail.com>
2022-08-18 11:37:11 -0300

    Primeiro commit

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+  public isLoading = false;
+  public apiError = false;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  public getPokemon() {
+    const id = this.activatedRoute.snapshot.params.id;
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+
+  }
+
+}
